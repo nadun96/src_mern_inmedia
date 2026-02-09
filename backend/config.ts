92d6698003818fe4cd26cfa36d359fe7cd60c371
nadun96 <nadunudaraka@gmail.com>
2026-02-09 09:23:25 +0530

    feat: update package.json and pnpm-lock.yaml to include mongoose and tsx
    - Changed the "dev" script in package.json to use tsx instead of ts-node.
    - Added mongoose as a dependency in package.json.
    - Added tsx as a devDependency in package.json.
    - Updated pnpm-lock.yaml to reflect the addition of mongoose and tsx.
    - Created a new config.ts file to manage MongoDB connection URI.
    - Integrated mongoose connection in server.ts with error handling.

---
+++ backend/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://nadun:XrtXsYWa7Lm8oS9R@cluster0.bzjd31q.mongodb.net/?appName=Cluster0",
+} 
+
--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,19 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import { config } from './config.js';
+import mongoose from 'mongoose';
+
+mongoose.connect(config.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+
 
 const app: express.Express = express();
 const PORT: number = 3000;
