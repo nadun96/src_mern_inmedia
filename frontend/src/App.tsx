541d4e651670295aa946e42d605741b2eae34d7f
nadun96 <nadunudaraka@gmail.com>
2026-02-10 23:41:38 +0530

    feat: add routing and pages for Home, Login, Profile, and SignUp; update Navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import { BrowserRouter , Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/profile' element={<Profile />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/signup' element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
-export default App
+export default App;
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
     return (
         <nav>
-        <div className ="nav-wrapper">
-        <a href="#" className="brand-logo">Logo</a>
+        <div className ="nav-wrapper white">
+        <Link to="/" className="brand-logo">Logo</Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><Link to="/login">Log In</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
         </ul>
         </div>
       </nav>
