77455926360646d92710a2858016f5db1d4bcca2
nadun96 <nadunudaraka@gmail.com>
2026-02-11 23:08:45 +0530

    feat: add email validation to Login and SignUp components; enhance user feedback with toast notifications

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,21 +1,75 @@
-import React from 'react'
-import './Login.css'
-type Props = {}
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
+import M from "materialize-css";
 
-const Login = (props: Props) => {
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    if (
+      !/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(
+        email,
+      )
+    ) {
+      M.toast({ html: "Invalid email format", classes: "red" });
+      return;
+    }
+    fetch("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          M.toast({ html: data.error, classes: "red" });
+        } else {
+          localStorage.setItem("jwt", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
+          M.toast({ html: "Login successful!", classes: "green" });
+          navigate("/");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div className='login-container '>
-      <div className='card login-card input-field'>
+    <div className="login-container ">
+      <div className="card login-card input-field">
         <h2>Login</h2>
-        <input  type="email" name="email" id="email" placeholder="example@example.com" />
-        <input  type="password" name="password" id="password" placeholder="********" />
-        <button className='btn waves-effect waves-light btn-large #6a1b9a purple darken-3'>Login</button>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="example@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="********"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="btn waves-effect waves-light btn-large #6a1b9a purple darken-3"
+          onClick={login}
+        >
+          Login
+        </button>
         <p>
-          Don't have an account? <a href="/signup">Sign Up</a>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
