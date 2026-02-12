13e2033dc9d87129e30621646249584e42044b0e
nadun96 <nadunudaraka@gmail.com>
2026-02-12 16:47:16 +0530

    feat: enhance routing logic in App component; implement user authentication check and redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,38 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import CreatePost from "./pages/CreatePost";
 import { reducer, initialState } from "./reducers/userReducer.ts";
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect, useContext } from "react";
 import { UserContext } from "./context/userContext.ts";
 
 const CustomRouteConfig = () => {
   const navigate = useNavigate();
+  const { state, dispatch } = useContext(UserContext);
+  const location = useLocation();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("user") || "null");
+    if (userInfo) {
+      dispatch({ type: "USER", payload: userInfo });
+    } else {
+      const isAuthRoute =
+        location.pathname === "/login" || location.pathname === "/signup";
+      if (!isAuthRoute) {
+        navigate("/login");
+      }
+    }
+  }, [dispatch, location.pathname, navigate]);
 
   return (
     <Routes>
