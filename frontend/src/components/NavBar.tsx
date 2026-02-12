e9764fd5029db8536c6f0f298237f1c7a973a50c
nadun96 <nadunudaraka@gmail.com>
2026-02-12 15:22:46 +0530

    feat: implement global state management using Context API and useReducer; add user authentication handling

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../context/userContext";
 
 const Navbar: React.FC = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const renderList = () => {
+    if (state) {
+      return [
+        <li key="profile">
+          <Link to="/profile">Profile</Link>
+        </li>,
+        <li key="create-post">
+          <Link to="/create-post">Create Post</Link>
+        </li>,
+        <li key="logout">
+          <button
+            className="btn #c62828 red darken-3"
+            onClick={() => {
+              localStorage.clear();
+              dispatch({ type: "LOGOUT" });
+              navigate("/login");
+            }}
+          >
+            Logout
+          </button>
+        </li>,
+      ];
+    } else {
+      return [
+        <li key="login">
+          <Link to="/login">Log In</Link>
+        </li>,
+        <li key="signup">
+          <Link to="/signup">Sign Up</Link>
+        </li>,
+      ];
+    }
+  };
+
   return (
     <nav>
       <div className="nav-wrapper white">
@@ -9,18 +47,7 @@ const Navbar: React.FC = () => {
           InMedi
         </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <Link to="/login">Log In</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/create-post">Create Post</Link>
-          </li>
+          {renderList()}
         </ul>
       </div>
     </nav>
